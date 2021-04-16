83116c4fbb47362d6f7309469687f87553710b87
Budi <boedixyz@gmail.com>
2021-04-16 13:38:23 +0700

    Remove unused module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,6 @@ import {
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { ProductModule } from './product/product.module';
-import { TransactionModule } from './transaction/transaction.module';
-import { CartModule } from './cart/cart.module';
-import { WishlistModule } from './wishlist/wishlist.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getConnectionOptions } from 'typeorm';
 import { LoggerMiddleware } from './shared/middlewares';
@@ -18,14 +14,7 @@ import { AuthModule } from './auth/auth.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 
-const ApplicationModule = [
-  UserModule,
-  ProductModule,
-  TransactionModule,
-  CartModule,
-  WishlistModule,
-  AuthModule,
-];
+const ApplicationModule = [UserModule, AuthModule];
 
 const VendorModule = [
   TypeOrmModule.forRootAsync({
